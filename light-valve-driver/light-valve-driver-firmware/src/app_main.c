7273271afe38531d2342bab4c2b09dee1b80db64
dot4qu <brian.team.jr@gmail.com>
2022-08-13 11:42:38 -0700

    Add call to our entrypoint in cubemx main

--- light-valve-driver/light-valve-driver-firmware/src/app_main.c
+++ light-valve-driver/light-valve-driver-firmware/src/app_main.c
@@ -1,4 +1,16 @@
-void app_main() {
+// Pull the pin name definitions from freertos main.h
+// #include "thirdparty/cubemx-light-valve-driver/Inc/main.h"
+
+void app_init(void) {
+}
+
+void app_start(void) {
+}
+
+void app_main(void) {
+    app_init();
+    app_start();
+
     while (1) {
     }
 }
