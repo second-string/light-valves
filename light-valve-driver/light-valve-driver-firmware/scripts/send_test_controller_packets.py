f9bde48b650fde2550b279a3115d8cec7ea509fd
dot4qu <brian.team.jr@gmail.com>
2022-09-17 13:25:57 +0200

    Refactor entire test packet script. Supports manual debugging packets as before, and also includes 6 new patterns that automatically address all 16 nodes for a single driver

--- light-valve-driver/light-valve-driver-firmware/scripts/send_test_controller_packets.py
+++ light-valve-driver/light-valve-driver-firmware/scripts/send_test_controller_packets.py
@@ -1,3 +1,4 @@
+import sys
 import argparse
 import serial
 import ctypes
@@ -11,16 +12,38 @@ class DriverPacket(ctypes.LittleEndianStructure):
                 ("addr_bits", uint8_t, 4),
                 ("node_data", uint8_t * 16)]
 
+class NodePacket:
+    def __init__(self, addr, data):
+        if addr > 0xF:
+            raise Exception("NodePacket addr can only be between 0x0 and 0xF")
+        if data > 0xF:
+            raise Exception("NodePacket data can only be between 0x0 and 0xF")
 
-def main(port, baudrate, num_driver_packets, driver_addr, node_addr, node_data):
-    try:
-        ser = serial.Serial(port, baudrate)
-    except Exception as e:
-        print(f"Failed to open serial port with exception {e}")
-        return
+        self.addr = addr
+        self.data = data
 
-    print(f"Sending {num_driver_packets} driver packets to driver address {driver_addr}.")
 
+pattern_choices = ["all_on", "all_off", "incrementing", "one_by_one", "full_fade", "moving_fade"]
+
+
+def send_driver_packet(ser, driver_addr, node_packet_list):
+
+    # Build the actual 8-bit values that will go to each node, i.e. 4 high bits addr 4 low bits data
+    node_data_bytes = []
+    for i in range(0, 16):
+        node_data_bytes.append((node_packet_list[i].addr << 4) | node_packet_list[i].data)
+
+    # Convert to ctypes array to match type in class fields
+    node_data_bytes_carray = (uint8_t * len(node_data_bytes))(*node_data_bytes)
+
+    print(f"Sending packet to driver address {driver_addr}.")
+    print([f"{packet.addr}, {packet.data}" for packet in node_packet_list])
+    packet = DriverPacket(0x6, driver_addr, node_data_bytes_carray)
+    packet_bytes = bytearray(packet)
+    ser.write(packet_bytes)
+
+
+def manual(ser, driver_addr, node_addr, node_data):
     node_addr_str = ""
     if (node_addr):
         node_addr_str = str(node_addr)
@@ -38,37 +61,113 @@ def main(port, baudrate, num_driver_packets, driver_addr, node_addr, node_data):
         node_data_str = "incrementing range(0, 16)"
         node_data_list = range(0, 16)
 
-        print(f"Each driver packet will hold 16 node_data bytes with address nibble {node_addr_str} and data nibble {node_data_str}")
-
-    # Build the actual 8-bit values that will go to each node, i.e. 4 high bits addr 4 low bits data
-    node_data_bytes = []
+    node_packets = []
     for i in range(0, 16):
-        node_data_bytes.append(((node_addr_list[i] & 0x0F) << 4) | (node_data_list[i] & 0x0F))
+        node_packets.append(NodePacket(node_addr_list[i], node_data_list[i]))
 
-    # Convert to ctypes array to match type in class fields
-    node_data_bytes_carray = (uint8_t * len(node_data_bytes))(*node_data_bytes)
+    print(f"Driver packet has 16 node_data bytes with address nibble {node_addr_str} and data nibble {node_data_str}")
+    send_driver_packet(ser, driver_addr, node_packets)
 
-    for i in range(0, num_driver_packets):
-        print(f"Sending driver packet {i + 1}")
 
-        packet = DriverPacket(0x6, driver_addr, node_data_bytes_carray)
-        packet_bytes = bytearray(packet)
-        ser.write(packet_bytes)
+def pattern(ser, driver_addr, pattern):
+    print(f"Displaying pattern '{pattern}'. If pattern repeats indefinitely, CTL+C to stop")
 
-    # Closing immediately will cut off the bytes still waiting to transmit
-    sleep(0.2)
-    ser.close()
+    pattern_change_interval_s = 1
+    node_packets = [NodePacket(i, 0x0) for i in range(0, 16)]
+
+    if pattern == "all_on":
+        for node_packet in node_packets:
+            node_packet.data = 0xF
+        send_driver_packet(ser, driver_addr, node_packets)
+    elif pattern == "all_off":
+        for node_packet in node_packets:
+            node_packet.data = 0x0
+        send_driver_packet(ser, driver_addr, node_packets)
+    elif pattern == "incrementing":
+        for i, node_packet in enumerate(node_packets):
+            node_packet.data = i
+        send_driver_packet(ser, driver_addr, node_packets)
+    elif pattern == "one_by_one":
+        node_on_index = 0
+        while 1:
+            node_packets[node_on_index].data = 0xF
+            send_driver_packet(ser, driver_addr, node_packets)
+            sleep(pattern_change_interval_s)
+            node_packets[node_on_index].data = 0x0
+            if node_on_index == 0xF:
+                node_on_index = 0x0
+            else:
+                node_on_index += 1
+    elif pattern == "full_fade":
+        node_data = 0
+        fade_dir = 1 # fading darker or lighter
+        while 1:
+            if node_packets[0].data == 0xF:
+                fade_dir = -1
+            elif node_packets[0].data == 0x0:
+                fade_dir = 1
+            for node_packet in node_packets:
+                node_packet.data += fade_dir
+
+            send_driver_packet(ser, driver_addr, node_packets)
+            sleep(pattern_change_interval_s)
+    elif pattern == "moving_fade":
+        current_node_index = 0
+        moving_dir = 1 # moving up or down node addresses
+        while 1:
+            node_packets[current_node_index].data = 0xF
+            for i in range(1, 0x10):
+                if current_node_index + i <= 0xF:
+                    node_packets[current_node_index + i].data = 0xF - i
+                if current_node_index - i >= 0x0:
+                    node_packets[current_node_index - i].data = 0xF - i
+
+            if current_node_index == 0xF:
+                moving_dir = -1
+            elif current_node_index == 0x0:
+                moving_dir = 1
+            current_node_index += moving_dir
+
+            send_driver_packet(ser, driver_addr, node_packets)
+            sleep(pattern_change_interval_s)
+    else:
+        raise Exception("Unsupported pattern!")
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("port", help="Serial port to transmit data out")
     parser.add_argument("-b", "--baudrate", help="Baudrate to use when connecting to serial port", default=115200)
-    parser.add_argument("-n", "--num_driver_packets", help="Number of packets to emit from controller to driver board(s)", default=1, choices=range(1, 16), type=int)
     parser.add_argument("-a", "--driver_addr", help="Driver address to send packet(s) to", default=0x1, choices=range(1, 16), type=int)
-    parser.add_argument("-x", "--node_addr", help=
-            "Node address nibble to use for all node_data within the packet. If not provided, node_data addresses will incremement from 0 to 15", choices=range(1, 16), type=int)
-    parser.add_argument("-d", "--node_data", help=
-            "Node data nibble to use for all node_data within the packet. If not provided, node_data data will incremement from 0 to 15", choices=range(1, 16), type=int)
+    subs = parser.add_subparsers(help="Choose what mode to execute the script in", dest="mode")
+
+    # Subparser for all 'manual' mode args
+    manual_parser = subs.add_parser("manual", help="Provide direct node addr/data for debugging")
+    manual_parser.add_argument("-x", "--node_addr", help=
+            "Node address nibble to use for all node_data within the packet. If not provided, all 16 nodes will be addresssed", choices=range(1, 16), type=int)
+    manual_parser.add_argument("-d", "--node_data", help=
+            "Node data nibble to use for all node_data within the packet. If not provided, node_data will match each node address its sent to", choices=range(1, 16), type=int)
+
+    # Subparser for all 'pattern' mode args
+    pattern_parser = subs.add_parser("pattern", help="Provide a pattern to display across all connected nodes")
+    pattern_parser.add_argument("pattern_choice", help="Pattern to display", choices=pattern_choices)
+
     args = parser.parse_args()
 
-    main(args.port, args.baudrate, args.num_driver_packets, args.driver_addr, args.node_addr, args.node_data)
+    try:
+        ser = serial.Serial(args.port, args.baudrate)
+    except Exception as e:
+        print(f"Failed to open serial port with exception: {e}")
+        sys.exit(1)
+
+    if args.mode == "manual":
+        manual(ser, args.driver_addr, args.node_addr, args.node_data)
+    elif args.mode == "pattern":
+        pattern(ser, args.driver_addr, args.pattern_choice)
+    else:
+        raise Exception("Shouldn't be able to get here with argparse choices")
+
+    # Closing immediately will cut off the bytes still waiting to transmit
+    sleep(0.2)
+    ser.close()
